18f4b9f59a7b21257020a4a5f0a3875772509366
Luka Tomic <tluka234@gmail.com>
2024-07-11 15:42:20 +0200

    Added Follow/Unfollow functionality

--- frontend/src/pages/UpdateProfilePage.jsx
+++ frontend/src/pages/UpdateProfilePage.jsx
@@ -27,11 +27,15 @@ export default function UpdateProfilePage() {
 	});
 	const fileRef = useRef(null);
 
+  const [updating, setUpdating ] = useState(false)
+
 	const showToast = useShowToast();
 
 	const { handleImageChange, imgUrl } = usePreviewImg();
 	const handleSubmit = async (e) => {
 		e.preventDefault();
+    if(updating) return
+    setUpdating(true)
 
 		try {
 			const res = await fetch(`/api/users/update/${user._id}`, {
@@ -51,7 +55,9 @@ export default function UpdateProfilePage() {
 			localStorage.setItem("user-threads", JSON.stringify(data));
 		} catch (error) {
 			showToast("Error", error, "error");
-		}
+		} finally {
+      setUpdating(false)
+    }
 	};
 	return (
 		<form onSubmit={handleSubmit}>
@@ -150,6 +156,7 @@ export default function UpdateProfilePage() {
 								bg: "green.500",
 							}}
 							type='submit'
+              isLoading={updating}
 						>
 							Submit
 						</Button>
