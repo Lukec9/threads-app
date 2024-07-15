fa457b3982a2639fb1f9f014a3d4cd3304dd8bf7
Luka Tomic <tluka234@gmail.com>
2024-07-15 13:15:13 +0200

    Chat UI Design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Container } from "@chakra-ui/react";
+import { Box, Container } from "@chakra-ui/react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import UserPage from "./pages/UserPage";
 import HomePage from "./pages/HomePage";
@@ -10,43 +10,50 @@ import userAtom from "./atoms/userAtom";
 import UpdateProfilePage from "./pages/UpdateProfilePage";
 import CreatePost from "./components/CreatePost";
 import NotFoundPage from "./pages/NotFound";
+import ChatPage from "./pages/ChatPage";
 
 function App() {
   const user = useRecoilValue(userAtom);
 
   return (
-    <Container maxW="620px">
-      <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={user ? <HomePage /> : <Navigate to="/auth" />}
-        />
-        <Route
-          path="/auth"
-          element={!user ? <AuthPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/update"
-          element={user ? <UpdateProfilePage /> : <Navigate to="/auth" />}
-        />
-        <Route
-          path="/:username"
-          element={
-            user ? (
-              <>
+    <Box position={"relative"} w={"full"}>
+      <Container maxW="620px">
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={user ? <HomePage /> : <Navigate to="/auth" />}
+          />
+          <Route
+            path="/auth"
+            element={!user ? <AuthPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/update"
+            element={user ? <UpdateProfilePage /> : <Navigate to="/auth" />}
+          />
+          <Route
+            path="/chat"
+            element={user ? <ChatPage /> : <Navigate to="/auth" />}
+          />
+          <Route
+            path="/:username"
+            element={
+              user ? (
+                <>
+                  <UserPage />
+                  <CreatePost />
+                </>
+              ) : (
                 <UserPage />
-                <CreatePost />
-              </>
-            ) : (
-              <UserPage />
-            )
-          }
-        />{" "}
-        <Route path="/:username/post/:pid" element={<PostPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </Container>
+              )
+            }
+          />{" "}
+          <Route path="/:username/post/:pid" element={<PostPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Container>
+    </Box>
   );
 }
 
