00dfcfb9b57ba56ba6aca32cec7087b63bd0c120
Luka Tomic <tluka234@gmail.com>
2024-07-20 08:32:04 +0200

    UserPage errors with posts

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -16,6 +16,7 @@ const UserPage = () => {
   const showToast = useShowToast();
 
   useEffect(() => {
+    setPosts([]);
     const getPosts = async () => {
       setFetchingPosts(true);
       try {
